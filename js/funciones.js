d6a9427ca51b82adaa22aebbe415e400ee82003f
Ulises Mendoza <69097655+Sumtrack98@users.noreply.github.com>
2020-10-05 17:38:18 -0600

    Popover-Tooltips

---
+++ js/funciones.js
@@ -0,0 +1,10 @@
+$(function () {
+  $('[data-toggle="popover"]').popover({
+    placement: "bottom",
+    trigger: "hover",
+  });
+});
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
