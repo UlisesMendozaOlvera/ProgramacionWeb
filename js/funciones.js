0d20f8117cce70554a28fc2103663540cd719271
Ulises Mendoza <69097655+Sumtrack98@users.noreply.github.com>
2020-10-13 20:43:43 -0600

    Prog. lado Cliente funcion Load();

--- js/funciones.js
+++ js/funciones.js
@@ -1,4 +1,50 @@
-$(function () {
+
+
+$(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+
+  $('[data-toggle="popover"]').popover({
+    placement: "bottom",
+    trigger: "hover",
+  });
+  
+modInicio();
+
+});
+
+
+function modInicio() {
+  $("#contenedor").empty();
+ 
+  
+  $("#contenedor").load("pages/inicio.html", function () {
+  });
+ 
+};
+function modCategorias() {
+  $("#contenedor").empty();
+  $("#contenedor").load("pages/Cat.html", function () {
+  });
+};
+function modComponentes() {
+  $("#contenedor").empty();
+  $("#contenedor").load("pages/comp.html", function () {
+  });
+};
+function modContactanos() {
+  $("#contenedor").empty();
+  $("#contenedor").load("pages/cont.html", function () {
+  });
+};
+function modNosotros() {
+  $("#contenedor").empty();
+  $("#contenedor").load("pages/nosotros.html", function () {
+  });
+};
+
+
+/*$(function () {
+
   $('[data-toggle="popover"]').popover({
     placement: "bottom",
     trigger: "hover",
@@ -8,3 +54,4 @@ $(function () {
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
 });
+*/
